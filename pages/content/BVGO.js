92789cf55b861282db23a43c969f967db6b51a9e
Israel Schulman <ischulman@ltvco.com>
2021-07-10 19:10:29 -0400

    Refactor step skipping logic. Some cleanup and doc comments.

--- pages/content/BVGO.js
+++ pages/content/BVGO.js
@@ -30,92 +30,44 @@ class BVGO {
     this.registerWizardReadyListener();
   }
 
+  /**
+   * Getter to determine whether steps are currently being skipped
+   */
   get areStepsBeingSkipped() {
     return this.stepsToSkip > 0;
-  }  
-
-  skipToStep(stepIndex) {
-    stepIndex *= 1;
-
-    const isFutureStep = stepIndex > this.currentWizardStepIndex;
-    const isCurrentStep = stepIndex === this.currentWizardStepIndex;
-    const isCurrentStepModalDisplayed = this.currentWizardStep.isModalDispled;
-
-    if(!this.areStepsBeingSkipped && ((isFutureStep) || (isCurrentStep && this.currentWizardStep.$modal && !isCurrentStepModalDisplayed))) {
-      this.stepsToSkip = this.getStepsToSkip(stepIndex);
-      this.skipStep();
-    }
   }
 
-  skipStep() {
-    setTimeout(() => {
-      this.stepsToSkip--;
-      BVGO.triggerBVGO();
+  /**
+   * Add event listeners to overlay elements
+   */
+  addListeners() {
+    this.onDragMouseDownHandler = this.onDragMouseDownHandler.bind(this);
+    this.onDragMouseMoveHandler = this.onDragMouseMoveHandler.bind(this);
+    this.onDragMouseUpHandler = this.onDragMouseUpHandler.bind(this);
+    this.toggleContainer = this.toggleContainer.bind(this);
+
+    if(this.containerMouseFades) {
+      this.container.addEventListener('mouseover', () => this.container.classList.remove('inactive'));
+      this.container.addEventListener('mouseout', () => this.container.classList.add('inactive'));
+    }
+
+    this.attachMouseOrTouchListener(this.titlebar, 'add', 'mousedown', this.onDragMouseDownHandler);
+
+    this.containerToggleBtn.addEventListener('click', this.toggleContainer);
+
+    [this.title, this.containerToggleBtn].forEach(elem => {
+      elem.addEventListener('mousedown', e => {
+        e.stopPropagation();
+      })
     });
+
+    this.title.addEventListener('click', BVGO.triggerBVGO);
   }
 
-  getStepsToSkip(stepIndex) {
-    let stepsToSkip = 0;
-
-    if(stepIndex === this.currentWizardStepIndex && !this.currentWizardStep.isModalDispled) {
-      stepsToSkip++;
-    } else {
-      for(let i = this.currentWizardStepIndex; i < stepIndex; i++) {
-        const step = this.wizardSteps[i];
-  
-        stepsToSkip += step.$modal && !step.isModalDispled ? 2 : 1;
-      }
-    }
-    console.log('stepsToSkip:', stepsToSkip);
-    return stepsToSkip;
-  }
-
-  onStartNextWizardStep(section, startNextStepOrig) {
-    const currentStep = section.getCurrentStep();
-    
-    startNextStepOrig.call(section);
-
-    
-    if(!currentStep.hide) {
-      console.log(currentStep.title, currentStep);
-      
-      this.currentWizardStep = currentStep;
-      this.currentWizardStepIndex++;
-
-      const bvgoTitleDataset = 'data-bvgo-overlay-step-title';
-      const bvgoStepClass = '.bvgo-overlay-wizard-step';
-      const bvgoModalClass = '.bvgo-overlay-wizard-step-modal';
-      const bvgoStepElement = document.querySelector(`[${bvgoTitleDataset}="${currentStep.title}"]`);
-      const bvgoActiveStepElement = document.querySelector(`${bvgoStepClass}.active`);
-
-      const onModalShown = () => {
-        document.querySelector(`[${bvgoTitleDataset}="${currentStep.title}"] ${bvgoModalClass}`)?.classList.add('active');
-
-        currentStep.$modal.off('show.bs.modal', onModalShown);
-
-        if(this.areStepsBeingSkipped) {
-          this.skipStep();
-        }
-      };
-
-      bvgoActiveStepElement?.classList.remove('active');
-      bvgoActiveStepElement?.classList.add('completed');
-      
-      bvgoStepElement?.classList.add('active');
-
-      if(currentStep.$modal) {
-        currentStep.$modal.on('show.bs.modal', onModalShown);
-      }
-
-      document.querySelector(`${bvgoModalClass}.active`)?.classList.add('completed');
-      document.querySelector(`${bvgoModalClass}.active`)?.classList.remove('active');
-
-      if(this.areStepsBeingSkipped) {
-        this.skipStep();
-      }
-    }
-  }
-
+  /**
+   * Add listener which waits for a message from the app code indication
+   * that the initialized wizard object has been added to the window
+   */
   registerWizardReadyListener() {
     window.addEventListener('message', e => {
       if(e.data.type === 'bvgo-app-wizard-ready') {
@@ -126,6 +78,215 @@ class BVGO {
     });
   }
 
+  /**
+   * Switches mouse events to comparable touch events if touch events are on the window
+   * 
+   * @param {Node} elem 
+   * @param {string} addOrRemove 
+   * @param {string} eventType 
+   * @param {Function} handler 
+   * @param {string|object} options 
+   */
+   attachMouseOrTouchListener(elem, addOrRemove, eventType, handler, options) {
+    const eventTypeMapping = {
+      mouseup: 'touchend',
+      mousemove: 'touchmove',
+      mousedown: 'touchstart',
+    };
+
+    if('ontouchstart' in window) {
+      eventType = eventTypeMapping[eventType];
+    }
+
+    elem[`${addOrRemove}EventListener`](eventType, handler, options);
+  }
+
+  /**
+   * Get clientX and clientY event properties depending on event type.
+   * 
+   * @param {Event} e MouseEvent or TouchEvent
+   * @returns {object} Object containing clientX and clientY values
+   */
+  getEventXYPosition(e) {
+    return {
+      clientX: e?.touches?.[0]?.clientX ?? e.clientX,
+      clientY: e?.touches?.[0]?.clientY ?? e.clientY,
+    };
+  }
+
+  /**
+   * Mouse down or touch start event  handler to initiate dragging of overlay
+   * 
+   * @param {MouseEvent|TouchEvent} e 
+   */
+  onDragMouseDownHandler(e) {
+    const { clientX, clientY } = this.getEventXYPosition(e);
+
+    this.dragStartX = clientX;
+    this.dragStartY = clientY;
+    this.dragStartOffsetLeft = this.container.offsetLeft;
+    this.dragStartOffsetTop = this.container.offsetTop;
+
+    this.attachMouseOrTouchListener(document, 'add', 'mousemove', this.onDragMouseMoveHandler, { passive: false });
+    this.attachMouseOrTouchListener(document, 'add', 'mouseup', this.onDragMouseUpHandler);
+  }
+
+  /**
+   * Mouse/Touch move event handler triggered while dragging the overlay
+   * 
+   * @param {MouseEvent|TouchEvent} e 
+   */
+  onDragMouseMoveHandler(e) {
+    const { clientX, clientY } = this.getEventXYPosition(e);
+    const x = clientX - this.dragStartX;
+    const y = clientY - this.dragStartY;
+    
+    this.container.style.left = (this.dragStartOffsetLeft + x) + 'px';
+    this.container.style.top = (this.dragStartOffsetTop + y) + 'px';
+
+    e.preventDefault();
+  }
+
+  /**
+   * Mouse up or touch end event handler to remove drag events when dragging is complete
+   */
+  onDragMouseUpHandler() {
+    this.attachMouseOrTouchListener(document, 'remove', 'mousemove', this.onDragMouseMoveHandler);
+    this.attachMouseOrTouchListener(document, 'remove', 'mouseup', this.onDragMouseUpHandler);
+  }
+
+  /**
+   * Skip to a specified future step index in the wizard
+   * 
+   * @param {number} stepIndex The target step index to skip to. Value is converted to a Number.
+   */
+  skipToStep(stepIndex) {
+    stepIndex *= 1;
+
+    // Skip to step index if steps aren't currently being skipped. Also ensure that the
+    // step index is a future step, including if it's equal to the current step index
+    // since the current step can still be awaiting displaying its modal.
+    if(!this.areStepsBeingSkipped && stepIndex >= this.currentWizardStepIndex) {
+      // this.stepsToSkip = this.getStepsToSkip(stepIndex);
+      this.setStepsToSkip(stepIndex);
+
+      if(this.stepsToSkip) {
+        this.skipStep();
+      }
+    }
+  }
+
+  /**
+   * Determines and sets stepsToSkip property based on the provided step index
+   * and how many steps it will take to get there, taking into account the
+   * modal portion of steps.
+   * 
+   * @param {number} stepIndex The target step index to skip to
+   */
+  setStepsToSkip(stepIndex) {
+    if(stepIndex < this.currentWizardStepIndex) return;
+
+    // If the current step was clicked, check if the step is still waiting
+    // to display its modal in which case only one step is to be skipped. 
+    if(stepIndex === this.currentWizardStepIndex && 
+      this.currentWizardStep.$modal && 
+      !this.currentWizardStep.isModalDispled) {
+
+      this.stepsToSkip = 1;
+      return;
+    }
+
+    // Loop through all steps from the current step through the clicked step index
+    // and increment the amount of steps to be skipped. Increment twice if a 
+    // modal is a part of a step and hasn't yet been displayed.
+    for(let i = this.currentWizardStepIndex; i < stepIndex; i++) {
+      const step = this.wizardSteps[i];
+
+      this.stepsToSkip += step.$modal && !step.isModalDispled ? 2 : 1;
+    }
+  }
+
+  /**
+   * Skip a step in the wizard. Called when a step is directly clicked on.
+   * In addition to triggering BVGO, this method decrements stepsToSKip
+   * and is execited on timeout which seems to be necessary for all
+   * steps to trigger.
+   */
+   skipStep() {
+    setTimeout(() => {
+      this.stepsToSkip--;
+      BVGO.triggerBVGO();
+    });
+  }
+
+  /**
+   * Method which intercepts each wizard section's startNextStep method.
+   * The section and saved startNextStep method is passed in and
+   * called so that wizard functionality doesn't break, but 
+   * allows for performingadditional logic exactly 
+   * when each step starts.
+   * 
+   * @param {object} section The section which this method is intercepting
+   * @param {Function} startNextStepOrig The original startNextStep method for this section
+   */
+  onStartNextWizardStep(section, startNextStepOrig) {
+    const currentStep = section.getCurrentStep();
+
+    startNextStepOrig.call(section);
+    
+    if(!currentStep.hide) {
+      console.log(currentStep.title, currentStep);
+
+      const bvgoTitleDataset = 'data-bvgo-overlay-step-title';
+      const bvgoStepClass = '.bvgo-overlay-wizard-step';
+      const bvgoModalClass = '.bvgo-overlay-wizard-step-modal';
+      const bvgoStepElement = document.querySelector(`[${bvgoTitleDataset}="${currentStep.title}"]`);
+      const bvgoActiveStepElement = document.querySelector(`${bvgoStepClass}.active`);
+      const bvgoActiveStepModalElement = document.querySelector(`${bvgoModalClass}.active`);
+
+      const onModalShown = () => {
+        document.querySelector(`[${bvgoTitleDataset}="${currentStep.title}"] ${bvgoModalClass}`)?.classList.add('active');
+
+        currentStep.$modal.off('show.bs.modal', onModalShown);
+
+        // Section startNextStep isn't triggered when the modal is displayed so make
+        // sure to continue skipping steps while skipping steps is still in
+        // progress and upon display of the modal.
+        if(this.areStepsBeingSkipped) {
+          this.skipStep();
+        }
+      };
+
+      this.currentWizardStep = currentStep;
+      this.currentWizardStepIndex++;
+
+      if(currentStep.$modal) {
+        currentStep.$modal.on('show.bs.modal', onModalShown);
+      }
+
+      // the next step is starting, so if there is currently an active
+      // step, remove its active class and add a completed class
+      bvgoActiveStepElement?.classList.remove('active');
+      bvgoActiveStepElement?.classList.add('completed');
+
+      // ...and do the same if there is a modal element in the completed step
+      bvgoActiveStepModalElement?.classList.remove('active');
+      bvgoActiveStepModalElement?.classList.add('completed');
+      
+      // add the active class to the next step
+      bvgoStepElement?.classList.add('active');     
+
+      // continue skipping steps while skipping steps is still in progress
+      if(this.areStepsBeingSkipped) {
+        this.skipStep();
+      }
+    }
+  }
+
+  /**
+   * Flatten all wizard steps and add them on the instance as this.wizardSteps.
+   * Builds all wizard step elements and appends them to the overlay.
+   */
   generateWizardSteps() {
     console.log('Generating wizard steps...');
 
@@ -180,79 +341,10 @@ class BVGO {
     }
   }
 
-  addListeners() {
-    this.onDragMouseDownHandler = this.onDragMouseDownHandler.bind(this);
-    this.onDragMouseMoveHandler = this.onDragMouseMoveHandler.bind(this);
-    this.onDragMouseUpHandler = this.onDragMouseUpHandler.bind(this);
-    this.toggleContainer = this.toggleContainer.bind(this);
-
-    if(this.containerMouseFades) {
-      this.container.addEventListener('mouseover', () => this.container.classList.remove('inactive'));
-      this.container.addEventListener('mouseout', () => this.container.classList.add('inactive'));
-    }
-
-    this.attachMouseOrTouchListener(this.titlebar, 'add', 'mousedown', this.onDragMouseDownHandler);
-
-    this.containerToggleBtn.addEventListener('click', this.toggleContainer);
-
-    [this.title, this.containerToggleBtn].forEach(elem => {
-      elem.addEventListener('mousedown', e => {
-        e.stopPropagation();
-      })
-    });
-
-    this.title.addEventListener('click', BVGO.triggerBVGO);
-  }
-
-  getEventXYPosition(e) {
-    return {
-      clientX: e.touches ? e.touches[0].clientX : e.clientX,
-      clientY: e.touches ? e.touches[0].clientY : e.clientY,
-    };
-  }
-
-  onDragMouseDownHandler(e) {
-    const { clientX, clientY } = this.getEventXYPosition(e);
-
-    this.dragStartX = clientX;
-    this.dragStartY = clientY;
-    this.dragStartOffsetLeft = this.container.offsetLeft;
-    this.dragStartOffsetTop = this.container.offsetTop;
-
-    this.attachMouseOrTouchListener(document, 'add', 'mousemove', this.onDragMouseMoveHandler, { passive: false });
-    this.attachMouseOrTouchListener(document, 'add', 'mouseup', this.onDragMouseUpHandler);
-  }
-
-  onDragMouseMoveHandler(e) {
-    const { clientX, clientY } = this.getEventXYPosition(e);
-    const x = clientX - this.dragStartX;
-    const y = clientY - this.dragStartY;
-    
-    this.container.style.left = (this.dragStartOffsetLeft + x) + 'px';
-    this.container.style.top = (this.dragStartOffsetTop + y) + 'px';
-
-    e.preventDefault();
-  }
-
-  attachMouseOrTouchListener(elem, addOrRemove, eventType, handler, options) {
-    const eventTypeMapping = {
-      mouseup: 'touchend',
-      mousemove: 'touchmove',
-      mousedown: 'touchstart',
-    };
-
-    if('ontouchstart' in window) {
-      eventType = eventTypeMapping[eventType];
-    }
-
-    elem[`${addOrRemove}EventListener`](eventType, handler, options);
-  }
-
-  onDragMouseUpHandler() {
-    this.attachMouseOrTouchListener(document, 'remove', 'mousemove', this.onDragMouseMoveHandler);
-    this.attachMouseOrTouchListener(document, 'remove', 'mouseup', this.onDragMouseUpHandler);
-  }
-
+  /**
+   * Toggle the container to hide or display the content section which
+   * would contain the wizards steps.
+   */
   toggleContainer() {
     this.isCollapsed = !this.isCollapsed;
 
@@ -265,8 +357,10 @@ class BVGO {
     }
   }
 
+  /**
+   * Builds the overlay and adds it to the page
+   */
   build() {
-    console.log('pageType:', this.pageType);
     this.container = document.createElement('div');
     this.container.classList.add('bvgo-overlay-container');
 
